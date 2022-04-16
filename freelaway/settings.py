f5b1414c09553bb77585c0bb27b57b3e5ad22d9f
navegantes <raphaelnavegantes@gmail.com>
2022-04-16 04:47:00 -0300

    deploy app 05

--- freelaway/settings.py
+++ freelaway/settings.py
@@ -21,7 +21,8 @@ django_heroku.settings(locals())
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
