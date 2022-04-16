aaa185e5db6beb61777024afbe5286186d740a46
navegantes <raphaelnavegantes@gmail.com>
2022-04-16 04:00:56 -0300

    deploy app 01

--- freelaway/settings.py
+++ freelaway/settings.py
@@ -15,6 +15,10 @@ import os
 from pathlib import Path
 from decouple import config
 
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
